903cdb7244f97068169b59278afeb2d4549a018b
MdSalman2022 <salman15-4523@diu.edu.bd>
2022-07-06 09:56:58 +0600

    herosection done

--- src/components/client/HeroSection/HeroSection.js
+++ src/components/client/HeroSection/HeroSection.js
@@ -1,24 +1,41 @@
 import React from 'react';
-import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
-import { Autoplay, Pagination } from 'swiper'
 import slider_1 from '../../../assets/slider/img1.jpg'
 import slider_2 from '../../../assets/slider/img2.jpg'
 import slider_3 from '../../../assets/slider/img3.jpg'
 import slider_4 from '../../../assets/slider/img4.jpg'
 import './HeroSection.css'
 
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
 
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+// import "./styles.css";
+
+// import required modules
+import { Autoplay, Pagination, Navigation } from "swiper";
 
 const HeroSection = () => {
     return (
         <>
-        <Swiper  slidesPerView={1}
-         autoplay={{
-             delay: 3000,
-         }}
-         modules={[Autoplay]}
-          pagination={true} modules={[Pagination]  } className="mySwiper">
+        <Swiper
+        spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Autoplay, Pagination, Navigation]}
+        className="mySwiper"
+      >
           <SwiperSlide><img src={slider_1} alt="" /></SwiperSlide>
           <SwiperSlide><img src={slider_2} alt="" /></SwiperSlide>
           <SwiperSlide><img src={slider_3} alt="" /></SwiperSlide>
